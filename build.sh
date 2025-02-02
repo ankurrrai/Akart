045718fea0fd35d721dc69c5557658226da8c656
Ankur Rai <133564443+ankurrrai@users.noreply.github.com>
2025-02-02 13:25:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+# Delete the database (if using SQLite)
+rm db.sqlite3
+
+# Delete migrations (except __init__.py)
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
